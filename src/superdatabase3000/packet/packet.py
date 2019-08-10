a3adb51f07940d69722f3de9e27464d58f4e50ab
JeanMax <mcanal@student.42.fr>
2019-08-10 20:40:34 +0200

    Fix packet payload

--- src/superdatabase3000/packet/packet.py
+++ src/superdatabase3000/packet/packet.py
@@ -1,9 +1,11 @@
 """
 packet[64]:    abcd            abcdefghabcdefghabcd        abcdefgh
                  ^               ^                           ^
-               canari[4]       checksum[20]                extra_payload_size[8]
+               canari[4]       checksum[20]                payload_size[8]
 
 
+                               payload_size
+               <------------------------------------------->
 
                abcdefghabcdefghabcdefghabcdefgh        [...]
                  ^                                       ^
@@ -25,51 +27,98 @@ PACKET_MIN_SIZE = (
     CANARI_SIZE + CHECKSUM_SIZE + INT_SIZE
     + PAYLOAD_MIN_SIZE
 )  # 64
+CHECKSUM_OFFSET = CANARI_SIZE + CHECKSUM_SIZE  # we'll start hasing from there
 
 STRUCT_FORMAT = (
     "!"
     "I"  # canari
     f"{CHECKSUM_SIZE}s"  # checksum
-    "Q"  # extra_payload_size
-    f"{PAYLOAD_MIN_SIZE}s"  # payload
+    "Q"  # payload_size
+    "{payload_size}s"  # payload: complete its size using format
 )
 
 
 Packet = collections.namedtuple(
     "Packet",
-    ["canari", "checksum", "extra_payload_size", "payload"]
+    ["canari", "checksum", "payload_size", "payload"]
 )
 
 
 def checksum(bytes_buf):
-    return hashlib.sha1(bytes_buf[CANARI_SIZE + CHECKSUM_SIZE:]).digest()
+    """TODO"""
+    return hashlib.sha1(bytes_buf[CHECKSUM_OFFSET:]).digest()
 
 
-def pack(payload):
-    payload = payload.zfill(PAYLOAD_MIN_SIZE)
+def verify_checksum(ctrl_checksum, bytes_buf):
+    """TODO"""
+    if ctrl_checksum != checksum(bytes_buf):
+        raise ValueError("packet: invalid checksum")
+    return True
+
+
+def pack(payload, with_checksum=True):
+    """TODO"""
+
     packet = Packet(
         canari=CANARI,
-        checksum=b'0' * CHECKSUM_SIZE,
-        extra_payload_size=len(payload) - PAYLOAD_MIN_SIZE,
-        payload=payload
+        checksum=b"\x00" * CHECKSUM_SIZE,
+        payload_size=len(payload),
+        payload=payload.ljust(PAYLOAD_MIN_SIZE, b"\x00")
     )
+    payload_size = max(packet.payload_size, PAYLOAD_MIN_SIZE)
     try:
-        bytes_buf = struct.pack(STRUCT_FORMAT, *packet)
+        bytes_buf = struct.pack(
+            STRUCT_FORMAT.format(payload_size=payload_size),
+            *packet
+        )
     except struct.error as e:
-        raise ValueError(f"Packet: {e}")
-    packet.checksum = checksum(bytes_buf)
-    bytes_buf = struct.pack(STRUCT_FORMAT, *packet)
+        raise ValueError(f"packet: {e}")
+
+    if with_checksum:
+        packet = packet._replace(checksum=checksum(bytes_buf))
+        bytes_buf = struct.pack(
+            STRUCT_FORMAT.format(payload_size=payload_size),
+            *packet
+        )
+
     return bytes_buf
 
 
-def unpack(bytes_buf):
+def unpack(bytes_buf, with_checksum=True):
+    """TODO"""
+
+    # first, we try to unpack as if it was a 64 bytes packet
     try:
-        packet = struct.unpack(STRUCT_FORMAT, bytes_buf)
-        packet = Packet(*packet)
+        packet = struct.unpack(
+            STRUCT_FORMAT.format(payload_size=PAYLOAD_MIN_SIZE),
+            bytes_buf[:PACKET_MIN_SIZE]
+        )
     except struct.error as e:
-        raise ValueError(f"unpack: {e}")
+        raise ValueError(f"packet: {e}")
+    packet = Packet(*packet)
     if packet.canari != CANARI:
-        raise ValueError("unpack: the canari is dead")
-    if not packet.extra_payload_size and packet.checksum != checksum(bytes_buf):
-        raise ValueError("unpack: invalid checksum")
-    return packet
+        raise ValueError("packet: the canari is dead")
+
+    # payload can fit in a 64 bytes packet: just verify checksum, then job done
+    if packet.payload_size <= PAYLOAD_MIN_SIZE:
+        if with_checksum:
+            verify_checksum(packet.checksum, bytes_buf)
+        packet = packet._replace(
+            payload=packet.payload[:packet.payload_size]
+        )
+        return packet
+
+    # packet is actually bigger than 64 bytes (extra_payload)
+    if len(bytes_buf) <= PACKET_MIN_SIZE:
+        return packet  # the payload is incomplete, and checksum not verified
+    try:
+        packet = struct.unpack(
+            STRUCT_FORMAT.format(payload_size=packet.payload_size),
+            bytes_buf
+        )
+    except struct.error as e:
+        raise ValueError(f"packet: {e}")
+    packet = Packet(*packet)
+    if with_checksum:
+        verify_checksum(packet.checksum, bytes_buf)
+    return packet  # complete packet with extra payload
---
+++ tests/test_packet.py
@@ -0,0 +1,24 @@
+import superdatabase3000.packet.packet as pckt
+
+
+def test_pack_unpack():
+    payload = b"hello"
+    bytes_buf = pckt.pack(payload)
+    assert len(bytes_buf) == pckt.PACKET_MIN_SIZE
+    packet = pckt.unpack(bytes_buf)
+    assert packet.payload == payload
+    assert packet.payload_size == len(payload)
+
+
+def test_big_pack_unpack():
+    payload = b"hello" * 1000
+    bytes_buf = pckt.pack(payload)
+    assert len(bytes_buf) > pckt.PACKET_MIN_SIZE
+    packet = pckt.unpack(bytes_buf)
+    assert packet.payload == payload
+    assert packet.payload_size
+
+    packet = pckt.unpack(bytes_buf[:pckt.PACKET_MIN_SIZE])
+    assert packet.payload_size == len(payload)
+    assert len(packet.payload) == pckt.PAYLOAD_MIN_SIZE
+    assert packet.payload == payload[:pckt.PAYLOAD_MIN_SIZE]
